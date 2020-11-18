61146a8fae43493eb70fca7e6b3568f86859af1d
NgoThanhHieu <66179085+NgoThanhHieu@users.noreply.github.com>
2020-11-18 21:36:14 +0100

    Add files via upload

---
+++ DATABAZE-Inserty.sql
@@ -0,0 +1,113 @@
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Honda','Nsx');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Nissan','370Z');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Audi','R8');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('BMV','I8');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Mercedes-Benz','SIs');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Tesla','Roadster');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Tesla','cybertruck');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Tesla','Model X');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Toyota','Supra');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Suzuki','S-Cross');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Kia','Carens');
+
+INSERT INTO cars (manufacturer, model)
+VALUES ('Kia','Carnival');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Ferrari','Ff');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Ferrari','Fxx');
+
+INSERT INTO car (manufacturer, model)
+VALUES ('Škoda','Octavia');
+
+SELECT COUNT(*) AS pocet_aut
+FROM car;
+
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Martin','Kokeš',35,'martin.kokes@ossp.cz');
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Karel','Nakládal',18,'karel.nakladal@student.ossp.cz')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Jakub','Vávrů',16,'jakub.vavru@student.ossp.cz');
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Milan','Ngo',16,'thanhhieu.ngo@student.ossp.cz')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Jan','Hladik',18,'jan.hladik@student.ossp.cz')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Jan','Šlechta',16,'Jan.slechta@student.ossp.cz');
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Michal','Plaček',16,'michal.placek@student.ossp.cz')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Libor','Pluháček',16,'libor.pluhacek@student,ossp,cz');
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Šimon','Siksta',16,'simon.siksta@student.ossp.cz')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Martin','Lédl',16,'martin.ledl@student.ossp.cz')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Sorin','Eni',16,'sorin.eni@student.ossp.cz');
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Zora','Blažejová',35,'zora.blazejova@ossp.cz')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Dana','Kolínská',35,'dana.kolinska@ossp.cz');
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Štěpán','Kolarovský',16,'stepan.kolarovsky@student.ossp.cz')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Anakin','Skywalker',26,'anakin.skywalker@student.ossp.cz');
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Darth','Vader',45,'darth.vader@ossp.cz')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Peter','Parker',20,'peter.parker@student.ossp')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Tony','Stark',52,'tony.stark@student.ossp.cz');
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Steven','Strange',36,'steven.strange@student.ossp.cz')
+
+INSERT INTO employees (name, lastname, age, email)
+VALUES ('Thanos','Titan',1 000,'thanos.titan@student.ossp.cz')
+
+SELECT COUNT(*) AS pocet_zamestnancu
+FROM employees;
+
